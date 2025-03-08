45ab9b23ae74232a7e819212457e56abfdacaa8c
강인혁 <ggh2260@gmail.com>
2025-03-08 18:01:56 +0900

    Rename: 패키지 구조 변경

--- backend/LocketDiscovery/src/main/java/com/locket/locketdiscovery/LocketDiscoveryApplication.java
+++ backend/LocketDiscovery/src/main/java/com/locket/locketdiscovery/LocketDiscoveryApplication.java
@@ -1,4 +1,4 @@
-package com.ssafy.locket;
+package com.locket.locketdiscovery;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
