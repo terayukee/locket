1c3564cf9a59360e75b8cfdea0f56ce6f00e5627
강인혁 <ggh2260@gmail.com>
2025-03-14 17:33:37 +0900

    Chore: 게이트웨이에서 스웨거 몰아보기 설정

---
+++ backend/LocketApiGateway/src/main/java/com/locket/apigateway/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.locket.apigateway.config;
+
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi paymentServiceApi() {
+        return GroupedOpenApi.builder()
+                .group("Locket Payment Service")
+                .pathsToMatch("/api/payment/**")
+                .build();
+    }
+
+    @Bean
+    public GroupedOpenApi userServiceApi() {
+        return GroupedOpenApi.builder()
+                .group("Locket User Service")
+                .pathsToMatch("/api/user/**")
+                .build();
+    }
+}
---
+++ backend/LocketPaymentService/src/main/java/com/locket/payment/config/SwaggerConfig.java
@@ -0,0 +1,18 @@
+package com.locket.payment.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Locket Payment API")
+                        .version("1.0")
+                        .description("결제 관련 API 문서"));
+    }
+}
