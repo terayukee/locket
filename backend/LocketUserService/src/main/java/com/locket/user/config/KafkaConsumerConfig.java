e6968d29877786af1d58d8d6474d5c75b87099fd
강인혁 <ggh2260@gmail.com>
2025-03-16 03:18:32 +0900

    Feat: User 서비스 카프카 컨슈머 세팅 완료 Related to: #S12P21D204-72

---
+++ backend/LocketUserService/src/main/java/com/locket/user/config/KafkaConsumerConfig.java
@@ -0,0 +1,38 @@
+package com.locket.user.config;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
+import org.springframework.kafka.core.ConsumerFactory;
+import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConsumerConfig {
+
+    @Bean
+    public ConsumerFactory<String, String> consumerFactory() {
+        Map<String, Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");  // Kafka 서버 주소
+        config.put(ConsumerConfig.GROUP_ID_CONFIG, "user-service-group"); // 컨슈머 그룹 ID
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest"); // 가장 오래된 메시지부터 처리
+
+        return new DefaultKafkaConsumerFactory<>(config);
+    }
+
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, String> kafkaListenerContainerFactory() {
+        ConcurrentKafkaListenerContainerFactory<String, String> factory =
+                new ConcurrentKafkaListenerContainerFactory<>();
+        factory.setConsumerFactory(consumerFactory());
+        return factory;
+    }
+}
---
+++ backend/LocketUserService/src/main/java/com/locket/user/config/KafkaTopicConfig.java
@@ -0,0 +1,5 @@
+package com.locket.user.config;
+
+public class KafkaTopicConfig {
+
+}
