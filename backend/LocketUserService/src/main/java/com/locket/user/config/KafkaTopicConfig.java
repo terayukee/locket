ed2accf0293ac6293aee8b8a0defd93c1f5a1a45
강인혁 <ggh2260@gmail.com>
2025-03-16 04:14:32 +0900

    Chore: 카프카 설정 변경 Related to: #S12P21D204-72

--- backend/LocketUserService/src/main/java/com/locket/user/config/KafkaTopicConfig.java
+++ backend/LocketUserService/src/main/java/com/locket/user/config/KafkaTopicConfig.java
@@ -1,5 +1,14 @@
 package com.locket.user.config;
 
+import org.apache.kafka.clients.admin.NewTopic;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class KafkaTopicConfig {
 
+    @Bean
+    public NewTopic paymentSuccessTopic() {
+        return new NewTopic("payment.success", 1, (short) 1);
+    }
 }
