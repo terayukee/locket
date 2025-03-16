63fa480a1408c79db52980cdfa1fd57882a17327
김기훈 <y2k16241@naver.com>
2025-03-16 20:31:56 +0900

    Init: Locket Android 프로젝트 초기 구조 설정

---
+++ Locket/app/src/main/java/com/example/locket/utils/CommonUtils.kt
@@ -0,0 +1,33 @@
+package com.example.locket
+
+import java.text.DecimalFormat
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
+import java.util.TimeZone
+
+object CommonUtils {
+    //천단위 콤마
+    fun makeComma(num: Int): String {
+        val comma = DecimalFormat("#,###")
+        return "${comma.format(num)} 원"
+    }
+
+    //날짜 포맷 출력
+    fun dateformatYMDHM(time:Date):String{
+        val format = SimpleDateFormat("yyyy.MM.dd. HH:mm", Locale.KOREA)
+        format.timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        return format.format(time)
+    }
+
+    fun dateformatYMD(time: Date):String{
+        val format = SimpleDateFormat("yyyy.MM.dd", Locale.KOREA)
+        format.timeZone = TimeZone.getTimeZone("Asia/Seoul")
+        return format.format(time)
+    }
+
+    fun formatLongToDate(longDate: Long): String {
+        val format = SimpleDateFormat("yyyy.MM.dd HH:mm", Locale.getDefault())  // 원하는 날짜 형식 지정
+        return format.format(Date(longDate))  // Long 값을 Date 객체로 변환 후 포맷 적용
+    }
+}
