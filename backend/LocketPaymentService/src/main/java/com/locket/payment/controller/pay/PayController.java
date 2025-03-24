a2c7c8dbb44f16490d2882343ce7dcac91db96bb
강인혁 <ggh2260@gmail.com>
2025-03-24 17:53:07 +0900

    feat: 결제 메서드 swagger 미등록 에러 해결

--- backend/LocketPaymentService/src/main/java/com/locket/payment/controller/pay/PayController.java
+++ backend/LocketPaymentService/src/main/java/com/locket/payment/controller/pay/PayController.java
@@ -12,6 +12,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +24,7 @@ public class PayController {
 
     private final PayService payService;
 
-//    @PostMapping("/nfc")
+    @PostMapping("/nfc")
     @Operation(summary = "결제", description = "결제를 처리합니다.")
     @ApiResponses({
             @ApiResponse(responseCode = "200", description = "결제 성공"),
