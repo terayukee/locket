87e57a3e4fbfeeb8d708cc4bc96391f3f2d65968
강인혁 <ggh2260@gmail.com>
2025-03-28 12:56:07 +0900

    chore: 카프카 컨슈머들 groupId에 각자 이름추가해서 충돌 방지

--- backend/LocketUserService/src/main/java/com/locket/user/config/KafkaConsumerConfig.java
+++ backend/LocketUserService/src/main/java/com/locket/user/config/KafkaConsumerConfig.java
@@ -42,6 +42,7 @@ public class KafkaConsumerConfig {
 		props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
 		props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
 
+
 		return new DefaultKafkaConsumerFactory<>(props, new StringDeserializer(), deserializer);
 	}
 
