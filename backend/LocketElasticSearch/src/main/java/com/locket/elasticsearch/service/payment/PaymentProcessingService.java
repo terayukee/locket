aa261dd03bf51669dc714323573be37324bd3e5b
강인혁 <ggh2260@gmail.com>
2025-03-30 00:27:56 +0900

    fix: 테스트를 위해 카테고리 분류 기능 off

--- backend/LocketElasticSearch/src/main/java/com/locket/elasticsearch/service/payment/PaymentProcessingService.java
+++ backend/LocketElasticSearch/src/main/java/com/locket/elasticsearch/service/payment/PaymentProcessingService.java
@@ -27,17 +27,17 @@ public class PaymentProcessingService {
 
         try {
             // FastAPI 호출 - Map으로 직접 요청/응답 처리
-            log.info("📤 Sending category classification request for store: {}", event.getStoreName());
-            Map<String, String> request = Map.of("storeName", event.getStoreName());
-
-            Map<String, Object> response = webClient.post()
-                    .uri("/api/category/classify")
-                    .bodyValue(Map.of("storeName", event.getStoreName()))
-                    .retrieve()
-                    .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
-                    .block();
-            log.info("📥 Received category classification response: {}", response);
-            log.info("📥 Category from response: {}", response.get("paymentCategory"));
+//            log.info("📤 Sending category classification request for store: {}", event.getStoreName());
+//            Map<String, String> request = Map.of("storeName", event.getStoreName());
+//
+//            Map<String, Object> response = webClient.post()
+//                    .uri("/api/category/classify")
+//                    .bodyValue(Map.of("storeName", event.getStoreName()))
+//                    .retrieve()
+//                    .bodyToMono(new ParameterizedTypeReference<Map<String, Object>>() {})
+//                    .block();
+//            log.info("📥 Received category classification response: {}", response);
+//            log.info("📥 Category from response: {}", response.get("paymentCategory"));
 
             // DTO -> Elasticsearch Entity로 변환
             PaymentHistory history = PaymentHistory.builder()
@@ -48,9 +48,9 @@ public class PaymentProcessingService {
                     .birthDate(event.getBirthDate())
                     .totalAmount(event.getTotalAmount())
                     .currency(event.getCurrency())
-                    .paymentCategory((String) response.get("paymentCategory"))  // API 응답으로 받은 카테고리 사용
-                    //.paymentCategory(event.getPaymentCategory())
-                    .needItemCheck((Boolean) response.get("needItemCheck"))
+//                    .paymentCategory((String) response.get("paymentCategory"))  // API 응답으로 받은 카테고리 사용
+                    .paymentCategory(event.getPaymentCategory())
+//                    .needItemCheck((Boolean) response.get("needItemCheck"))
                     .paymentMerchant(event.getPaymentMerchant())
                     .storeName(event.getStoreName())                  // ✅ 매장명
                     .receiptUploaded(event.isReceiptUploaded())       // ✅ 영수증 업로드 여부
