4c3db8c169a28375ca3674872a4ccf672b808cfc
강인혁 <ggh2260@gmail.com>
2025-03-31 15:13:06 +0900

    fix: 결제 테스트 코드 원상복구

--- backend/LocketPaymentService/src/main/java/com/locket/payment/service/pay/PayService.java
+++ backend/LocketPaymentService/src/main/java/com/locket/payment/service/pay/PayService.java
@@ -263,7 +263,7 @@ public class PayService {
                     .orders(orderDetails)
                     .build();
 
-//        paymentProducer.sendPaymentSuccessEvent(event);
+            paymentProducer.sendPaymentSuccessEvent(event);
 
             return ResponseEntity.ok(PaymentResponse.builder()
                     .transactionId(transaction.getPaymentTransactionId().toString())
