9a2425a848f6d4e2219a023b23c4e10406be39e2
안세호 <aash711@naver.com>
2025-04-01 21:01:27 +0900

    fix: 피드백 요청 DTO 필드명 매핑 수정

--- backend/LocketUserService/src/main/java/com/locket/user/domain/budget/dto/BudgetFeedbackRequest.java
+++ backend/LocketUserService/src/main/java/com/locket/user/domain/budget/dto/BudgetFeedbackRequest.java
@@ -1,5 +1,6 @@
 package com.locket.user.domain.budget.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Builder;
 import lombok.Getter;
 import java.util.Map;
@@ -7,7 +8,8 @@ import java.util.Map;
 @Getter
 @Builder
 public class BudgetFeedbackRequest {
-    private Map<String, Integer> categoryAmount;
+    @JsonProperty("categoryAmount")
+    private Map<String, Integer> totalCategoryAmount;
     private BudgetMonthlyStatusDto budgetStatus;
     private String userJob;
 }
