c65e542857084da366144d50c355f36b9d41cf79
안세호 <aash711@naver.com>
2025-04-03 22:18:08 +0900

    fix: BudgetFeedbackResponse Redis 직렬화 지원

--- backend/LocketUserService/src/main/java/com/locket/user/domain/budget/dto/BudgetFeedbackResponse.java
+++ backend/LocketUserService/src/main/java/com/locket/user/domain/budget/dto/BudgetFeedbackResponse.java
@@ -1,10 +1,14 @@
 package com.locket.user.domain.budget.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class BudgetFeedbackResponse {
     private String nickname;
     private String feedback;
