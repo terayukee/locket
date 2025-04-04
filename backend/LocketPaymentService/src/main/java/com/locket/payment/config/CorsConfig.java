9e76572cdbbbcca8ad81d21d8430ffb3869eea88
강인혁 <ggh2260@gmail.com>
2025-04-04 21:52:41 +0900

    feat: 엘라스틱 서치 서비스 스웨거 URL 변경

--- backend/LocketPaymentService/src/main/java/com/locket/payment/config/CorsConfig.java
+++ backend/LocketPaymentService/src/main/java/com/locket/payment/config/CorsConfig.java
@@ -1,4 +1,4 @@
-package com.locket.elasticsearch.config;
+package com.locket.payment.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
