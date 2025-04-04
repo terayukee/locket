10de2a922b8f68aa76360bda274f8964c84a7f31
hay6586 <hay6586@gmail.com>
2025-04-04 13:41:55 +0900

    feat: 예산 설정 수정

--- Locket/presentation/src/main/java/com/ssafy/locket/presentation/finance/fragment/budget/BudgetFragment.kt
+++ Locket/presentation/src/main/java/com/ssafy/locket/presentation/finance/fragment/budget/BudgetFragment.kt
@@ -3,21 +3,31 @@ package com.ssafy.locket.presentation.finance.fragment.budget
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.activityViewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.fragment.findNavController
 import com.ssafy.locket.presentation.R
 import com.ssafy.locket.presentation.base.BaseFragment
 import com.ssafy.locket.presentation.common.viewmodel.FinanceNavigationState
 import com.ssafy.locket.presentation.common.viewmodel.MainViewModel
 import com.ssafy.locket.presentation.databinding.FragmentBudgetBinding
+import com.ssafy.locket.presentation.finance.viewmodel.FinanceSharedViewModel
 import com.ssafy.locket.presentation.utils.CommonUtils
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 import java.time.LocalDate
 import java.time.YearMonth
 
+@AndroidEntryPoint
 class BudgetFragment : BaseFragment<FragmentBudgetBinding>(
     FragmentBudgetBinding::bind,
     R.layout.fragment_budget
 ) {
     private val mainViewModel : MainViewModel by activityViewModels()
+    private val financeSharedViewModel: FinanceSharedViewModel by activityViewModels()
+    private val today = LocalDate.now()
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
@@ -43,13 +53,21 @@ class BudgetFragment : BaseFragment<FragmentBudgetBinding>(
         binding.tvBudget.text = resources.getString(R.string.finance_budget_left, CommonUtils.makeComma(budget))
         binding.tvRecommendBudgetToday.text = resources.getString(R.string.finance_won, CommonUtils.makeComma((budget/getDaysInCurrentMonth())*today.dayOfMonth))
 
+        viewLifecycleOwner.lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                financeSharedViewModel.selectedYearMonth.collect {
+                    binding.btnBudgetSet.isEnabled = it.year == today.year && it.monthValue == today.monthValue
+                }
+            }
+        }
+
         binding.btnBudgetSet.setOnClickListener {
             findNavController().navigate(R.id.action_financeFragment_to_editBudgetFragment)
         }
     }
+
     private fun getDaysInCurrentMonth(): Int {
-        val currentDate = LocalDate.now()
-        return YearMonth.of(currentDate.year, currentDate.month).lengthOfMonth()
+        return YearMonth.of(today.year, today.month).lengthOfMonth()
     }
 
     override fun onDestroyView() {
