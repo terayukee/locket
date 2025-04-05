cd29f1d52944ebb51205448d0a4fe341cb21c385
강인혁 <ggh2260@gmail.com>
2025-04-06 00:39:43 +0900

    fix: PaymentService측 FeignClient IP 직접 호출로 불법 변경 시도

--- backend/LocketPaymentService/src/main/java/com/locket/payment/feign/PaymentHistoryFeignClient.java
+++ backend/LocketPaymentService/src/main/java/com/locket/payment/feign/PaymentHistoryFeignClient.java
@@ -9,7 +9,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.List;
 import java.util.Map;
 
-@FeignClient(name = "elasticsearch-service", contextId = "paymentHistoryClient")
+//@FeignClient(name = "elasticsearch-service", contextId = "paymentHistoryClient")
+@FeignClient(
+        name = "elasticsearch-service",
+        contextId = "paymentHistoryClient",
+        url = "http://172.26.5.222:8083" // 실제 컨테이너 IP 및 포트
+)
 public interface PaymentHistoryFeignClient {
 
     // ✅ 카드 기준 월간 총 결제 금액
