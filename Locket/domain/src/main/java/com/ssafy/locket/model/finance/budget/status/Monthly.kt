a23078f4155f43b80a57f1b481966f20decc0316
김기훈 <y2k16241@naver.com>
2025-04-06 17:51:50 +0900

    fix 예산용 progress를 double으로 변경

--- Locket/domain/src/main/java/com/ssafy/locket/model/finance/budget/status/Monthly.kt
+++ Locket/domain/src/main/java/com/ssafy/locket/model/finance/budget/status/Monthly.kt
@@ -2,7 +2,7 @@ package com.ssafy.locket.model.finance.budget.status
 
 data class Monthly(
     val month: Int,
-    val progress: Int,
+    val progress: Double,
     val remaining: Int,
     val spent: Int,
     val target: Int,
