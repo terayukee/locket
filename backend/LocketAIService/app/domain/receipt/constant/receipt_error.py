811c9aa600cacc38854efae5c971ca0b21506f3f
안세호 <aash711@naver.com>
2025-04-07 23:58:43 +0900

    feat: ocr 데이터 유효성 검사 추가

--- backend/LocketAIService/app/domain/receipt/constant/receipt_error.py
+++ backend/LocketAIService/app/domain/receipt/constant/receipt_error.py
@@ -9,6 +9,9 @@ class ReceiptErrorCode(Enum):
     LOW_RESOLUTION = 1005       # 낮은 해상도
     CORRUPTED_FILE = 1006       # 파일 손상
     PDF_TOO_MANY_PAGES = 1007   # PDF 페이지 초과
+    AMOUNT_MISMATCH = 1008        # 결제 금액 불일치
+    TOTAL_AMOUNT_MISMATCH = 1009  # 총액과 항목 합계 불일치
+    PAYMENT_NOT_FOUND = 1010    # 결제 내역 없음
 
     # 500 에러 (1500-1999)
     OCR_ERROR = 1501            # OCR 처리 실패
@@ -24,6 +27,9 @@ class ReceiptErrorMessage(Enum):
     LOW_RESOLUTION = "이미지 해상도가 너무 낮습니다"
     CORRUPTED_FILE = "파일이 손상되었습니다"
     PDF_TOO_MANY_PAGES = "PDF는 1페이지만 처리 가능합니다"
+    AMOUNT_MISMATCH = "결제 금액이 영수증 금액과 일치하지 않습니다"
+    TOTAL_AMOUNT_MISMATCH = "영수증 총액이 카테고리 합계와 일치하지 않습니다"
+    PAYMENT_NOT_FOUND = "해당하는 결제 내역을 찾을 수 없습니다"
 
     # 500 에러 메시지
     OCR_ERROR = "영수증 인식 처리에 실패했습니다"
